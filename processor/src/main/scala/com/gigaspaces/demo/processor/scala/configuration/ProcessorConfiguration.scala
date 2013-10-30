2356f641b9f38b570ec58c9dafff992f5dbadfbf
Jeremy Prime <mr.nonpc@gmail.com>
2013-10-30 16:22:56 +0000

    Scala Spring configuration for feeder

---
+++ processor/src/main/scala/com/gigaspaces/demo/processor/scala/configuration/ProcessorConfiguration.scala
@@ -0,0 +1,39 @@
+package com.gigaspaces.demo.processor.scala.configuration
+
+import com.gigaspaces.demo.common.scala.Data
+import org.springframework.context.annotation.{Bean, Configuration}
+import com.gigaspaces.demo.processor.scala.Processor
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer
+
+/**
+ * @author Jez
+ */
+@Configuration
+class ProcessorConfiguration {
+
+  /**
+   * Template object bean to identify non-processed Data objects
+   * @return
+   */
+  @Bean
+  def unprocessedDataTemplate = {
+    val template = new Data()
+    template.setProcessed(false)
+    template
+  }
+
+  /**
+   * The data processor Spring bean for the configuration
+   * @return
+   */
+  @Bean
+  def dataProcessor = new Processor
+
+  /**
+   * Spring property configurer which allows us to use system properties (such as user.name).
+   * @return the property placeholder configurer bean
+   */
+  @Bean
+  def propertiesConfigurer = new PropertyPlaceholderConfigurer
+
+}
