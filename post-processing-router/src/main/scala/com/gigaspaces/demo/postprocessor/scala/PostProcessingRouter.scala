575f148c8a7bc214156cb3a4159cfd8a9df60728
Jeremy Prime <mr.nonpc@gmail.com>
2013-10-31 18:19:34 +0000

    Annotation based config for the router to show the simplification. Would be nice if we could make the routingTemplate a Scala predicate somehow

--- post-processing-router/src/main/scala/com/gigaspaces/demo/postprocessor/scala/PostProcessingRouter.scala
+++ post-processing-router/src/main/scala/com/gigaspaces/demo/postprocessor/scala/PostProcessingRouter.scala
@@ -1,14 +1,19 @@
 package com.gigaspaces.demo.postprocessor.scala
 
 import java.util.logging.Logger
-import scala.beans.BeanProperty
 import org.openspaces.events.adapter.SpaceDataEvent
 import com.gigaspaces.demo.common.scala.Data
-import org.springframework.beans.factory.annotation.{Autowired, Qualifier}
+import org.openspaces.events.polling.{ReceiveHandler, Polling}
+import org.openspaces.events.{EventTemplate, TransactionalEvent, EventDriven}
+import com.j_spaces.core.client.SQLQuery
+import java.lang.Boolean._
+import org.openspaces.events.asyncpolling.AsyncPolling
+import org.openspaces.events.polling.receive.SingleTakeReceiveOperationHandler
 
 /**
  * @author Jez
  */
+@EventDriven @Polling @TransactionalEvent
 class PostProcessingRouter {
 
   val log= Logger.getLogger(this.getClass.getName)
@@ -20,12 +25,23 @@ class PostProcessingRouter {
   var siRoutedFlow: AnyRef = null
   */
 
+  @EventTemplate
+  def routingTemplate = new SQLQuery[Data] (classOf[Data], "processed = ? AND sent = ?", TRUE, FALSE)
+
   @SpaceDataEvent
   def sendData(data: Data): Data = {
     Thread.sleep(100)
     data.sent = true
     log.info(" ------ SENT : " + data)
     data
+  }
+
+  @ReceiveHandler
+  def receiveHandler = {
+    val receiveHandler = new SingleTakeReceiveOperationHandler
+    receiveHandler.setNonBlocking (true)
+    receiveHandler.setNonBlockingFactor(10); // Intentional batching up
+    receiveHandler
   }
 
 }
